a360c2cbcb6f8f413db5ecd6f9f37b333c641611
him41111 <him41111@gmail.com>
2023-08-07 17:48:52 +0000

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ nodaemon=true
 logfile=/var/log/supervisord.log
 [program:agent]
 user=root
-command=/app/nezha-agent -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} --skip-conn --skip-procs --tls -d
+command=/app/nezha-agent -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} --skip-conn --skip-procs --tls
 autostart=true
 autorestart=true
 [program:php]
